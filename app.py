97ed92b5d8d033bd005042f1760ed9dab1eb6a1b
jordan cornwell <jordancornwell@dyn-192-168-232-190.wifi.wou.edu>
2019-10-17 10:29:21 -0700

    added the route for GETing a specific activity from the activity log

--- app.py
+++ app.py
@@ -6,25 +6,32 @@ import json
 app = Flask(__name__)
 
 activity_log = [
-	{
-		'id': 0,
-		'user_id': 1,
-		'username': 'john',
-		'timestamp': datetime.utcnow(),
-		'details': 'important stuff here',
-	},
-	{
-		'id': 1,
-		'user_id': 2,
-		'username': 'yoko',
-		'timestamp': 'john',
-		'details': 'important stuff here',
-	},
+    {
+        'id': 0,
+        'user_id': 1,
+        'username': 'john',
+        'timestamp': datetime.utcnow(),
+        'details': 'important stuff here',
+    },
+    {
+        'id': 1,
+        'user_id': 2,
+        'username': 'yoko',
+        'timestamp': datetime.utcnow(),
+        'details': 'important stuff here',
+    },
 ]
 
 @app.route('/api/activity_log', methods=['GET'])
-def get_activites():
-	return jsonify({'activity_log': activity_log})
+def get_activity():
+    return jsonify({'activity_log': activity_log})
+
+@app.route('/api/activity_log/<int:activity_id>', methods=['GET'])
+def get_specific_activity(activity_id):
+    # activity = [lambda x: x['id'] == activity_id, activity_log]
+    for item in filter(lambda x: x['id'] == activity_id, activity_log):
+        return jsonify({'activity_log': item})
+
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)

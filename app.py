ccb8414ddff260ac39006cc4c25ec89c4cbec28b
jordan cornwell <jordancornwell@dyn-192-168-232-190.wifi.wou.edu>
2019-10-17 10:47:41 -0700

    added the route to POST a new activity to the activity log

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #hey there..
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from datetime import datetime
 import json
 
@@ -22,15 +22,30 @@ activity_log = [
     },
 ]
 
+
 @app.route('/api/activity_log', methods=['GET'])
 def get_activity():
     return jsonify({'activity_log': activity_log})
+
 
 @app.route('/api/activity_log/<int:activity_id>', methods=['GET'])
 def get_specific_activity(activity_id):
     # activity = [lambda x: x['id'] == activity_id, activity_log]
     for item in filter(lambda x: x['id'] == activity_id, activity_log):
         return jsonify({'activity_log': item})
+
+
+@app.route('/api/activity_log', methods=['POST'])
+def post_activity():
+    new_activity = {
+        'id': activity_log[-1]['id'] + 1,
+        'user_id': activity_log[-1]['user_id'] + 1,
+        'username': request.json['username'],
+        'timestamp': datetime.utcnow(),
+        'details': request.json['details']
+    }
+    activity_log.append(new_activity)
+    return jsonify({'activity_log': new_activity})
 
 
 if __name__ == '__main__':
